a989452c2401ad78ac366556603481a0d40ceefd
Deepak4003 <jangiddee999@gmail.com>
2025-01-21 17:19:46 +0000

    variable

---
+++ basics/variable.js
@@ -0,0 +1,14 @@
+ const accountId= 733976
+ let accountEmail ="jangiddee999@gmail.com"
+ var accountPassword ="12345"
+ let accountState;
+ accountCity ="jaipur"
+ accountEmail ="dee@.com"
+ accountPassword="772797"
+ accountCity="pune"
+ console.log(accountId);
+ /*
+ prefre to use let in place of var
+ */
+
+ console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);

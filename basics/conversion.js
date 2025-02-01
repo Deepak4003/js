ef262730a75c7dd65cef47197104b2705ecce497
Deepak4003 <jangiddee999@gmail.com>
2025-02-01 21:30:39 +0000

    conversion

---
+++ basics/conversion.js
@@ -0,0 +1,19 @@
+let score = "33"
+console.log(typeof score);
+let valueinnumber = Number(score);
+console.log(typeof valueinnumber);
+
+//"33"==33
+//"33abc"=>NaN
+//true=>1
+
+let isloggedin = 1
+let booleanislogeedin = Boolean(isloggedin)//convert one into true
+console.log(booleanislogeedin)
+
+
+//""=>false
+//deepak=>true
+let somenumber = 33
+let stringnumber = String(somenumber)
+console.log(stringnumber);

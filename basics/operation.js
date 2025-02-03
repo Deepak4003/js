960b2068d7cf777a7e775994250f26a7153c32e2
Deepak4003 <jangiddee999@gmail.com>
2025-02-03 20:12:36 +0000

    operations

---
+++ basics/operation.js
@@ -0,0 +1,25 @@
+let value = 3
+let negvalue = -value
+console.log(negvalue)
+console.log(2 + 2)
+console.log(2 ** 3)//if  use two ** bwork as power
+let str1 = "Deepak"
+let str2 = " jangid"
+let str3 = str1 + str2
+console.log(str3)
+console.log(1 + "2")
+console.log("1" + 2 + 2)
+
+console.log(+true)
+
+console.log(+"")
+
+
+
+
+
+
+
+
+
+

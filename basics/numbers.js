8b21339bf6974d1c97f208c7eae4002011203fbf
Deepak4003 <jangiddee999@gmail.com>
2025-02-05 03:45:19 +0000

    numbermath

---
+++ basics/numbers.js
@@ -0,0 +1,33 @@
+const score=400
+
+const balance =new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(1));
+
+
+const other=23.9877
+// console.log(other.toPrecision(3));
+
+
+const hundreds=10000
+// console.log(hundreds.toLocaleString('en-IN'));
+Number.MAX_VALUE
+
+//maths
+
+console.log(math);
+console.log(math.abs(-4));
+//math.round(4.6)=>4
+//math.ceil(4.2)=>5
+//min,max;
+console.log(math.random()*10+1);//value b/w 0 and 1 use in games
+console.log((math.random()*10)+1);//add 1 to avoid zero
+
+
+const min=10;
+const max=20;
+
+console.log(math.floor(math.random() *(max-min+1))+min)//VVI
+

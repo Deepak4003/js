1fe992168d2201444b20cafd69dd77db235ae606
Deepak4003 <jangiddee999@gmail.com>
2025-02-05 02:22:35 +0000

    stackheap

--- basics/datatypessummary.js
+++ basics/datatypessummary.js
@@ -1,4 +1,22 @@
 //primitive data type
-const id=Symbol('123')
-const anotherid=Symbol('123')
-console.log(id==anotherid)
+const id = Symbol('123')
+const anotherid = Symbol('123')
+console.log(id == anotherid)
+
+//stack memory(primitive) ,heap memory(non primitive)
+
+let myyoutube = "Deepakjangid"//goes in stack
+let anothername = myyoutube
+anothername = "chaiaurcode"//its change in copy of value not in real value so treal value remai same
+console.log(anothername)
+console.log(myyoutube)
+
+
+
+let userone={
+    email:"dee@gmail.com",
+    upi:"@ybl"
+}
+let usertwo=userone
+usertwo.email="hiiii";
+console.log(usertwo)

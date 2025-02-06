cdc71a34ca7919a8403a6918a8cd453827054748
Deepak4003 <jangiddee999@gmail.com>
2025-02-06 03:15:18 +0000

    date

---
+++ basics/dateandtime.js
@@ -0,0 +1,12 @@
+let newData = new Date();
+console.log(newData);
+console.log(newdate.getMonth()+1);
+console.log(newdate.getDay());
+
+`${newData.getDay()}`
+
+newData.toLocaleString('default',{
+    weekday:"long",
+    //timeZone:''
+    
+})
